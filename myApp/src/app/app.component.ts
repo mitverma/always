dbac4251037937db01cde657e23fe5231c6f9cee
mitverma <amitwohlig@gmail.com>
2017-09-15 19:13:49 +0530

    changes

--- myApp/src/app/app.component.ts
+++ myApp/src/app/app.component.ts
@@ -17,7 +17,7 @@ import { OrderPage } from '../pages/order/order';
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
-  rootPage: any = FaqPage;
+  rootPage: any = LoginPage;
 
   pages: Array<{title: string, component: any}>;
 

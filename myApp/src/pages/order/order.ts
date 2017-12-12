d36aa03f6ad2f8f8b1cf22a94a80a67d09537c60
mitverma <amitwohlig@gmail.com>
2017-12-12 13:27:28 +0530

    work in progress

--- myApp/src/pages/order/order.ts
+++ myApp/src/pages/order/order.ts
@@ -19,19 +19,58 @@ import { NgForm } from '@angular/forms';
   providers : [AllPostProvider]
 })
 export class OrderPage {
-
+  showAccessoryType = false;
 	orderForm = {};
-	orderFormData = {};
-	message = false;
-  orderOption: string[];
-  orderOption = ['A51 - Pre Filter inlet pipe 1 meter','A52 - Ionizer inlet pipe 1 meter','A53 - Acidic water  ionizer output pipe  1 meter','A54 - Filter','Alkaline water output hose'];
+  orderFormData = {};
+  message = false;
+  orderOption: any[];
+  orderAccessory : any[] = [];
   // orderOption = ['amit','vikas','aniruddh'];
 
   constructor(public navCtrl: NavController, public navParams: NavParams,public FormBuilder: FormBuilder,public http:Http, public orderProvider: AllPostProvider) {
+    // this.orderOption = ['A51 - Pre Filter inlet pipe 1 meter','A52 - Ionizer inlet pipe 1 meter','A53 - Acidic water  ionizer output pipe  1 meter','A54 - Filter','Alkaline water output hose'];
+    this.orderOption = [{
+      'name':'A51 - Pre Filter inlet pipe 1 meter',
+      'quantity': '1',
+      'value': false
+    },{
+      'name':'A52 - Ionizer inlet pipe 1 meter',
+      'quantity': '1',
+      'value': false
+    }, {
+      'name':'A53 - Acidic water  ionizer output pipe  1 meter',
+      'quantity': '1',
+      'value': false
+    }, {
+      'name':'A54 - Filter',
+      'quantity': '1',
+      'value': false
+    },{
+      'name':'Alkaline water output hose',
+      'quantity': '1',
+      'value': false
+    }]
+  }
+  addAccessory(order,i){
+    console.log(order,'order',this.orderAccessory,i);
+    if(order.value == false){
+      console.log('push');
+      this.orderAccessory.push(order);
+      this.orderOption[i].value = true;
+      this.orderOption[i].class = "active";
+    }else{
+      console.log('slice');
+      this.orderAccessory.splice(order,1);
+      this.orderOption[i].value = false;
+      this.orderOption[i].class = "";
+    }
+    console.log(order,'order',this.orderAccessory,i);
   }
 
   formOrder(orderForm){   
     console.log(orderForm,'orderForm');
+    orderForm.accessory = this.orderAccessory;
+    console.log(orderForm,'new value');
   	this.orderProvider.saveOrder(this.orderForm).subscribe(data=>{
   		this.message = data;
       if(data.message == "successfully Registered Support Request"){
